5fbaef4ec48f5952eb81ac42dffead4a62036a0e
lucascesarnf <lucascesarnf@hotmail.com>
2017-06-09 13:02:56 -0300

    Get start

---
+++ Pods/Target Support Files/Pods-WeatherMapAPI/Pods-WeatherMapAPI-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WeatherMapAPIVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WeatherMapAPIVersionString[];
+
---
+++ Pods/Target Support Files/Pods-WeatherMapAPITests/Pods-WeatherMapAPITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WeatherMapAPITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WeatherMapAPITestsVersionString[];
+
